98598c40adc0b082594cd391bd0b4c78c1f8cc31
Ayomide <heyayomideadebisi@gmail.com>
2025-10-14 09:29:25 +0100

    old commit

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,48 @@
-pub mod todos;
-pub mod books_api;
-pub mod error;
+// pub mod todos;
+// pub mod books_api;
+// pub mod error;
 
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+// use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 // use crate::books_api::books;
 
-#[tokio::main]
-async fn main() {
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
-                format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into()
-            }),
-        )
-        .with(tracing_subscriber::fmt::layer())
-        .init();
+// #[tokio::main]
+// async fn main() {
+//     tracing_subscriber::registry()
+//         .with(
+//             tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+//                 format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into()
+//             }),
+//         )
+//         .with(tracing_subscriber::fmt::layer())
+//         .init();
 
-    // books::check();
+//     // books::check();
 
-    // Compose the routes
-    let app = todos::create_router();
+//     // Compose the routes
+//     let app = todos::create_router();
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
-    tracing::debug!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
+//     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+//         .await
+//         .unwrap();
+//     tracing::debug!("listening on {}", listener.local_addr().unwrap());
+//     axum::serve(listener, app).await.unwrap();
+// }
+
+fn main() {
+    let text = String::from("The time is 10:00pm");
+    println!("Welcome to rust, {text}");
+
+    let number = 7;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    match number {
+        0 => println!("zero"),
+        1 => println!("one"),
+        _ => println!("something else"), // exhaustive wildcard. any other matches
+    }
 }
-
